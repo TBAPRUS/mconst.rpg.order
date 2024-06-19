045a1287fd42dab1ce2ba1d6c062a4788966b946
Constantineu <mol.kostya@mail.ru>
2024-06-20 02:13:04 +0500

    Spring Initializr

---
+++ src/test/java/mconst/rpg/order/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package mconst.rpg.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
